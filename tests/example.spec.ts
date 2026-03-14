1f67dc869307e6c4a3973720cd062569746b4b41
Santosh Dasari <priyakasantosh@Santoshs-MacBook-Air.local>
2026-03-14 17:01:14 +0530

    Commented the first test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -14,7 +14,6 @@ test('get started link', async ({ page }) => {
   await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(
-    page.getByRole('heading', { name: 'Installation' }),
+  await expect(page.getByRole('heading', { name: 'Installation' }),
   ).toBeVisible();
 });
